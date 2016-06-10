3d7121ab9f5188cbcee8645dba49beec6feb48b2
jacobbraun <jacobbraun@users.noreply.github.com>
2016-06-10 13:02:33 -0500

    Update dresden-fls-pp.groovy

--- devicetypes/sticks18/dresden-fls-pp.src/dresden-fls-pp.groovy
+++ devicetypes/sticks18/dresden-fls-pp.src/dresden-fls-pp.groovy
@@ -21,7 +21,7 @@
 
 metadata {
 	// Automatically generated. Make future change here.
-	definition (name: "Dresden FLS-PP", namespace: "sticks18", author: "Scott G") {
+	definition (name: "Dresden FLS-PP-Mod", namespace: "jacobbraun", author: "Scott G, modified by jacobbraun") {
 		capability "Switch Level"
 		capability "Actuator"
 		capability "Color Control"
